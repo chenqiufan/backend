74d7f9e2b18edd98253a5704813a040f1aeef587
chenqiufan <1334607406@qq.com>
2020-09-08 08:10:09 +0800

    Basic building

--- apps/user_api_v1/views.py
+++ apps/user_api_v1/views.py
@@ -1,10 +1,13 @@
 # coding = utf-8
-from flask import Blueprint
+from flask import Blueprint,request,session
 
 from ..models import User
 api_bp = Blueprint('user_api_v1',__name__)
 
 
-@api_bp.route('/')
+@api_bp.route('/login',methods=['POST'])
 def login():
+    session["name"] = ""
+    data = request.data
+    print(data)
     return "successed"
